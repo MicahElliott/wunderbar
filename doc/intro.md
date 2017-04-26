fff4e69a3a63bf21b34d999b29afead6ce78cf7b
Micah Elliott <mde@MicahElliott.com>
2017-04-26 13:03:18 -0700

    init (lein new)

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to wunder
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/wunder/core_test.clj
@@ -0,0 +1,7 @@
+(ns wunder.core-test
+  (:require [clojure.test :refer :all]
+            [wunder.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
